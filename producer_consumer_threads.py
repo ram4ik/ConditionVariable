e2e594c8f523163367227c6c7ca0917939b7f782
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-24 15:18:49 +0300

    finish program appropriately

--- producer_consumer_threads.py
+++ producer_consumer_threads.py
@@ -14,11 +14,15 @@ def soup_producer():
         print('Served Bowl #', str(i), '- remaining capacity:', \
               serving_line.maxsize-serving_line.qsize())
         time.sleep(0.2)  # time to serve a bowl of soup
+    serving_line.put_nowait('no soup for you!')
+    serving_line.put_nowait('no soup for you!')
 
 
 def soup_consumer():
     while True:
         bowl = serving_line.get()
+        if bowl == 'no soup for you!':
+            break
         print('Ate', bowl)
         time.sleep(0.3)  # time to eat a bowl of soup
 
