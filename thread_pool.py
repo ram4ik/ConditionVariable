2b173456ed0c906956446eef1d2ac3e101868306
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-24 16:42:15 +0300

    process pool executor

--- thread_pool.py
+++ thread_pool.py
@@ -1,5 +1,5 @@
 import threading
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 
 def vegetable_chopper(vegetable_id):
@@ -8,7 +8,6 @@ def vegetable_chopper(vegetable_id):
 
 
 if __name__ == '__main__':
-    pool = ThreadPoolExecutor(max_workers=5)
-    for vegetable in range(100):
-        pool.submit(vegetable_chopper, vegetable)
-    pool.shutdown()
+    with ProcessPoolExecutor(max_workers=5) as pool:
+        for vegetable in range(100):
+            pool.submit(vegetable_chopper, vegetable)
